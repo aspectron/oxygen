3def8b69052dd27fe92f7983d97d7eb4d662e87a
Pavel Medvedev <pmedvedev@gmail.com>
2013-10-26 01:55:29 +0400

    Adding event callback functions into the Oxygen test script

--- rte/test.js
+++ rte/test.js
@@ -4,6 +4,8 @@ var log = require("log");
 
 var oxygen = require("oxygen");
 
+var screen_size = oxygen.get_screen_size();
+log.info("Screen size: " + screen_size.width + "x" + screen_size.height);
 
 log.info("Creating window...");
 
@@ -18,7 +20,24 @@ var window = oxygen.window({
 
 log.info("Window created...");
 
+window.on('resize', function(size)
+  { log.info("width: " + size.width + " height: " + size.height);});
+
+window.on('close', function()
+  { log.info("window closed");});
+
+window.on('mousemove', function(e) { log.info(e);});
+window.on('mousedown', function(e) { log.info(e);});
+window.on('mouseup', function(e) { log.info(e);});
+window.on('mousewheel', function(e) { log.info(e);});
+window.on('mouseclick', function(e) { log.info(e);});
+window.on('keydown', function(e) { log.info(e);});
+window.on('keyup', function(e) { log.info(e);});
+window.on('char', function(e) { log.info(e);});
+
+/*
 dpc(25000, function(){
 	log.info("Destroying window...");
 	window.destroy();
 })
+*/
